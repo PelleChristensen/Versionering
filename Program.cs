5be41cfc314361061763b842df2e165dcb39b196
PELC <PELC@LU62-61660-PELC>
2024-08-21 11:52:50 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Versionering
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
